dd0dbaf2baba1535e2c1c8501075b5c6a4e2bbe3
He Taiyu <het2002@outlook.com>
2019-03-16 17:36:07 +0800

    .

---
+++ src/main/Config.java
@@ -0,0 +1,78 @@
+package main;
+
+import java.io.Serializable;
+
+public class Config implements Serializable {
+
+    private boolean isRandomTimes=true;
+
+    private boolean isNameChoose=true;
+
+    private boolean ignorePast=true;
+    private int chosenTime=120;
+    private short speed=80;
+
+    private short minNumber;
+    private short maxNumber;
+
+
+    public boolean isIgnorePast() {
+        return ignorePast;
+    }
+
+    public boolean isRandomTimes() {
+        return isRandomTimes;
+    }
+
+    public boolean isNameChoose() {
+        return isNameChoose;
+    }
+
+    public int getChosenTime() {
+        return chosenTime;
+    }
+
+    public short getMaxNumber() {
+        return maxNumber;
+    }
+
+    public short getMinNumber() {
+        return minNumber;
+    }
+
+    public short getSpeed() {
+        return speed;
+    }
+
+    public void setNameChoose(boolean nameChoose) {
+        isNameChoose = nameChoose;
+    }
+
+    public void setChosenTime(int chosenTime) {
+        this.chosenTime = chosenTime;
+    }
+
+    public void setIgnorePast(boolean ignorePast) {
+        this.ignorePast = ignorePast;
+    }
+
+    public void setRandomTimes(boolean randomTimes) {
+        isRandomTimes = randomTimes;
+    }
+
+    public void setMaxNumber(short maxNumber) {
+        this.maxNumber = maxNumber;
+    }
+
+    public void setMinNumber(short minNumber) {
+        this.minNumber = minNumber;
+    }
+
+    public void setSpeed(short speed) {
+        this.speed = speed;
+    }
+
+
+
+}
+
---
+++ src/main/Data.java
@@ -0,0 +1,121 @@
+package main;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Data {
+    private List<String> nameList;
+    private int listSize = 0;
+
+
+    File dataFile =new File("D:\\DM_Master_sources-master\\data");
+
+    //不做注释了，自己慢慢看。：）
+
+    //------------------------------------------------------
+    public Data(){
+
+        try{
+
+            if(dataFile.exists()!=true){
+                dataFile.createNewFile();
+                nameList= new ArrayList<>();
+                return;
+            }
+
+            ObjectInputStream ois =new ObjectInputStream(new FileInputStream(dataFile));
+            this.nameList=(ArrayList)ois.readObject();
+            listSize=nameList.size();
+            System.out.println(listSize);
+
+        }catch (Exception e){
+            nameList=new ArrayList<>();
+            e.printStackTrace();
+        }
+
+
+
+    }
+
+    //------------------------------------------------------
+    public void add(String text){
+        String[] temp;
+
+        if(text.contains("。")){
+            temp=text.split("。");
+            for(int i=0;i<temp.length;i++)
+                nameList.add(temp[i]);
+        }else {
+            nameList.add(text);
+            listSize=nameList.size();
+        }
+    }
+    //------------------------------------------------------
+    public String get(int i){
+        if(i<listSize-1)
+            return null;
+        else
+            return nameList.get(i);
+    }
+
+    //------------------------------------------------------
+    public int getSize(){
+        return nameList.size();
+    }
+
+    //------------------------------------------------------
+    public void delete(String name){
+        if(nameList.isEmpty())
+            return;
+        nameList.remove(name);
+        listSize=nameList.size();
+
+        for (String myString :nameList.toArray(new String[0])) {
+            System.out.println(myString);
+        }
+    }
+
+    //------------------------------------------------------
+    public boolean isEmpty(){
+        return  nameList.isEmpty();
+    }
+
+    //------------------------------------------------------
+    public String randomGet(){
+        //int i=(int)(1+Math.random()*(nameList.size()));
+        int i=(int)(Math.random()*((nameList.size()-1)));
+        return  nameList.get(i);
+    }
+
+    //------------------------------------------------------
+    public String[] getAll(){
+        return nameList.toArray(new String[0]);
+    }
+
+    //------------------------------------------------------
+    public int saveToFile(){
+
+        try{
+            ObjectOutputStream oos =new ObjectOutputStream(new FileOutputStream(dataFile));
+            oos.writeObject(nameList);
+            oos.close();
+            return 0;
+        }catch (Exception e){
+            e.printStackTrace();
+            return -1;
+        }
+
+    }
+
+    //------------------------------------------------------
+    public void deleteAll(){
+        nameList.clear();
+    }
+
+}
+
+
+
