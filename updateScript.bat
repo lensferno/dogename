2a0132d4f8a301368b550f2f2e99b7ac7ef607a4
het2002 <het2002@outlook.com>
2019-05-18 13:42:44 +0800

    为升级功能做准备

--- updateScript.bat
+++ updateScript.bat
@@ -1,6 +1,16 @@
 @echo off
+@echo ############################################
+@echo #                                          #
+@echo #                                          #
+@echo #              DM MASTER升级                #
+@echo #                                          #
+@echo #                                          #
+@echo ############################################
+@echo 
+@echo DM master正在升级，请不要关闭此窗口.....
 taskkill /im DMmaster.exe
 del DM_master.exe
+del DMmaster.exe
 rename update.tmp DMmaster.exe
 start DMmaster
 exit
