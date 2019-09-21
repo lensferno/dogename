e82449a21ffc25501b4302c5f56049cb0b08e458
hety <het2002@outlook.com>
2019-09-21 18:06:50 +0800

    改善不同系统中的路径识别

--- src/main/App.java
+++ src/main/App.java
@@ -1,16 +1,39 @@
 package main;
 
+import java.io.File;
+
 public class App {
     public String APP_LOCA;
+    private boolean debug=true;
     public App()
     {
-        if(System.getProperty("os.name").toLowerCase().contains("window"))
-            APP_LOCA=this.getClass().getProtectionDomain().getCodeSource().getLocation().getPath().substring(1).replace("/","\\");
-        else
-            APP_LOCA=this.getClass().getProtectionDomain().getCodeSource().getLocation().getPath();
+	
+	File file = new File(this.getClass().getProtectionDomain().getCodeSource().getLocation().getPath());
+	debug = file.isFile();
 
+        if(System.getProperty("os.name").toLowerCase().contains("window")) {
+            
+            APP_LOCA=this.getClass().getProtectionDomain().getCodeSource().getLocation().getPath().substring(1).replace("/","\\");
+            
+            if(!debug) {
+                String[] temp=APP_LOCA.split("\\");
+                APP_LOCA=APP_LOCA.replace(temp[temp.length-1], "");
+            }
+            
+        }else {
+            
+            APP_LOCA=this.getClass().getProtectionDomain().getCodeSource().getLocation().getPath();
+            
+            if(!debug) {
+                String[] temp=APP_LOCA.split("/");
+                APP_LOCA=APP_LOCA.replace(temp[temp.length-1], "");
+            }
+        }
+        
+        
         System.out.println("[INFO]Working in:"+APP_LOCA);
+        System.gc();
 
     }
 
-            }
+}
