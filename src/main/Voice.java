52c3a6fd4d6ee3091769f136714934c19213db84
het <het2002@outlook.com>
2019-11-06 11:33:07 +0800

    void

--- src/main/Voice.java
+++ src/main/Voice.java
@@ -42,7 +42,7 @@ public class Voice {
 
         checkNet();
         if(net){
-            //checkToken();
+            checkToken();
         }
 
 
@@ -66,7 +66,9 @@ public class Voice {
 
     void getToken(){
         try{
-            token=new Gson().fromJson(Common.getHtml(TOKEN_URL+"?grant_type=client_credentials&client_id="+API_KEY+"&client_secret="+SEC_KEY,true),Token.class);
+            token=new Gson().fromJson("{\"access_token\":\"24.86c56b964c3e14596dcc80e21d74fd24.2592000.1574998125.282335-17531281\",\"session_key\":\"9mzdA5wjKQQPSG1GToYpIVb1dGzEgMrv9UDeBWuXOJi9se\\/pfjkph2Lmpl9PQd0S6SBK4RHU7zjt5DaAluz\\/zQjPcKbmxQ==\",\"scope\":\"audio_voice_assistant_get brain_enhanced_asr audio_tts_post public brain_all_scope picchain_test_picchain_api_scope wise_adapt lebo_resource_base lightservice_public hetu_basic lightcms_map_poi kaidian_kaidian ApsMisTest_Test\\u6743\\u9650 vis-classify_flower lpq_\\u5f00\\u653e cop_helloScope ApsMis_fangdi_permission smartapp_snsapi_base iop_autocar oauth_tp_app smartapp_smart_game_openapi oauth_sessionkey smartapp_swanid_verify smartapp_opensource_openapi smartapp_opensource_recapi fake_face_detect_\\u5f00\\u653eScope vis-ocr_\\u865a\\u62df\\u4eba\\u7269\\u52a9\\u7406 idl-video_\\u865a\\u62df\\u4eba\\u7269\\u52a9\\u7406\",\"refresh_token\":\"25.a851090e2bd267f70f8186f23ab1bbb7.315360000.1887766125.282335-17531281\",\"session_secret\":\"6f439d43b796b04298ca1f9dfd32f1d4\",\"expires_in\":2592000}\n",Token.class);
+
+            //token=new Gson().fromJson(Common.getHtml(TOKEN_URL+"?grant_type=client_credentials&client_id="+API_KEY+"&client_secret="+SEC_KEY,true),Token.class);
             token.setExpTime();
         }catch (Exception e){
             token=null;
@@ -137,7 +139,7 @@ public class Voice {
                public void run() {
                    try{
                        String[] b ={"1","3","106"};
-                       String URL="http://tsn.baidu.com/text2audio?lan=zh&ctp=1&cuid=abcdxxx&tok=24.f44505f2d4604cf88e71cb8328c633b3.2592000.1574825643.282335-17531281&tex="+ URLEncoder.encode(name,"utf-8")
+                       String URL="http://tsn.baidu.com/text2audio?lan=zh&ctp=1&cuid=abcdxxx&tok="+token.getAccess_token()+"&tex="+ URLEncoder.encode(name,"utf-8")
                                +"&vol=0&per="+b[new Random().nextInt(b.length)]+"&spd=5&pit=4&aue=6";
                        System.out.println(URL);
                         URL sourcesURL = new URL(URL);
@@ -147,6 +149,8 @@ public class Voice {
 
                         InputStream stream = connection.getInputStream();
 
+                        if(connection.getHeaderField("Content-type").contains("json"))
+                            return;
 
                        if(!cacheDir.exists())
                            cacheDir.mkdir();
