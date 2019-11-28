623084f4d4a58e077a6537bb4fff43b9ae56e616
het <het2002@outlook.com>
2019-11-28 11:39:06 +0800

    增加一点“系统抽样”的东西

--- src/main/SystemSimpleMethod.java
+++ src/main/SystemSimpleMethod.java
@@ -1,12 +1,10 @@
 package main;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Random;
+import java.util.*;
 
-public class SystemSimpleMethod {
+public class systemSimpleMethod {
     Map group;
-    List<String> sysMethodList;
+    ArrayList<String> sysMethodList = new ArrayList<>();
     int groupAmount;
     int groupLength;
     int currentGroup;
@@ -14,4 +12,40 @@ public class SystemSimpleMethod {
     List ignoredGroupName;
     List removedList;
     Random rand;
+
+    void mixUpList() {
+    }
+
+    public void makeGroup(ArrayList nameList) {
+        removedList.clear();
+        ignoredGroupName.clear();
+        sysMethodList.clear();
+        sysMethodList.addAll(nameList);
+        mixUpList();
+
+        groupAmount = (int) Math.sqrt(sysMethodList.size());
+        int removedAmount = sysMethodList.size() % groupAmount;
+        int temp = 0;
+
+        for (int i = 0; i < removedAmount; i++) {
+            temp = rand.nextInt(sysMethodList.size());
+            removedList.add(sysMethodList.get(temp));
+            sysMethodList.remove(temp);
+        }
+
+        group = new HashMap<Integer, ArrayList>();
+        groupLength = (int) (sysMethodList.size() / groupAmount);
+
+        ArrayList tempList=null;
+        for (int i = 0; i<groupAmount;i++){
+            group.put(i, new ArrayList());
+            for (int a = 0;a<groupLength;a++){
+                tempList=(ArrayList) group.get(i);
+                tempList.add(sysMethodList.get(groupLength * 2));
+
+            }
+        }
+        currentNameNumber = 0;
+        currentGroup = 0;
+    }
 }
