50a15f7d1489817e9f6a8b78f291c3bc302a6162
het <het2002@outlook.com>
2020-01-08 17:17:17 +0800

    开始做好升级器

---
+++ updater/cmake-build-debug/CMakeFiles/updater.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/updater.dir/main.cpp.obj"
+  "libupdater.dll.a"
+  "updater.exe"
+  "updater.exe.manifest"
+  "updater.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/updater.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
