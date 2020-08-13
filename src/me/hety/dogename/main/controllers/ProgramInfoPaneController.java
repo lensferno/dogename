c97771a141a383459826db1304d17905836bbc70
He Taiyu <Het2002@outlook.com>
2020-08-14 00:23:22 +0800

    换了一下程序信息按钮的位置 另外四个文件工程文件的diff请忽略

--- src/me/hety/dogename/main/controllers/ProgramInfoPaneController.java
+++ src/me/hety/dogename/main/controllers/ProgramInfoPaneController.java
@@ -12,6 +12,11 @@ import javafx.scene.text.Font;
 import me.hety.dogename.main.DataReleaser;
 import me.hety.dogename.main.DialogMaker;
 
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
 public class ProgramInfoPaneController extends VBox {
 
     @FXML
@@ -44,5 +49,20 @@ public class ProgramInfoPaneController extends VBox {
         new DialogMaker(rootPane).creatDialogWithOneBtn("开源协议（LGPL v3）",textArea);
     }
 
+    @FXML
+    void showHelp(ActionEvent event) {
+
+    }
+
+    @FXML
+    void viewCode(ActionEvent event) {
+        try {
+            Desktop.getDesktop().browse(new URI("https://github.com/eatenid/dogename"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
